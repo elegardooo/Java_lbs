69f5e93de3d111396213f0d44f22da482ff0a078
elegardooo <1lagodich@mail.ru>
2024-02-25 00:35:59 +0300

    123

--- src/test/java/com/lagodich/textqrconvertor/TextQrConvertorApplicationTests.java
+++ src/test/java/com/lagodich/textqrconvertor/TextQrConvertorApplicationTests.java
@@ -3,12 +3,15 @@ package com.lagodich.textqrconvertor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TextQrConvertorApplicationTests {
 
 	@Test
 	void contextLoads() {
-		TextQrConvertorApplicationTests myClass = new TextQrConvertorApplicationTests();
+		int result = 1 + 2;
+		assertEquals(3, result, "1 + 2 should equal 3");
 	}
 
 }
