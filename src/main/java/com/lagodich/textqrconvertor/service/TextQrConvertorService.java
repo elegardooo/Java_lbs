9cf3255b9cda2cdc3da6ee30d4419aeac4006eb3
elegardooo <1lagodich@mail.ru>
2024-02-28 16:59:56 +0300

    123

--- src/main/java/com/lagodich/textqrconvertor/service/TextQrConvertorService.java
+++ src/main/java/com/lagodich/textqrconvertor/service/TextQrConvertorService.java
@@ -1,5 +1,6 @@
 package com.lagodich.textqrconvertor.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
@@ -9,17 +10,20 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class TextQrConvertorService {
 
     private TextQrConvertorService() {}
-    private static final String API_KEY = "9e538d02fdmsh143dd83847c491fp1e9e2bjsn10018d801db522";
-    private static final String BASE_API_URL = "http://api.qrserver.com/v1/create-qr-code/?data=";
+    @Value("${external.api-key.QrCode}")
+    private String apiKey;
+
+    @Value("${external.api.urlQrCode}")
+    private String baseApiUrl;
     private static final String BEARER_HEADER = "Bearer %s";
     private static final String BEARER_HEADER_AUTH_TYPE = "Authorization";
 
     @Component
-    public static class QrTextService {
+    public  class QrTextService {
         public byte[] qrCode(String qrCodeContent) {
             HttpHeaders headers = new HttpHeaders();
-            headers.set(BEARER_HEADER_AUTH_TYPE, BEARER_HEADER.formatted(API_KEY));
-            return WebClient.create(BASE_API_URL + qrCodeContent)
+            headers.set(BEARER_HEADER_AUTH_TYPE, BEARER_HEADER.formatted(apiKey));
+            return WebClient.create(baseApiUrl + qrCodeContent)
                     .get()
                     .accept(MediaType.IMAGE_PNG)
                     .headers(h -> h.addAll(headers))
