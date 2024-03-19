5ae34602d38c1ea90459821f4f264387b5813c0f
elegardooo <1lagodich@mail.ru>
2024-03-19 23:59:02 +0300

    @Query added

--- src/main/java/com/lagodich/textqrconvertor/repository/QrCodeColorRepo.java
+++ src/main/java/com/lagodich/textqrconvertor/repository/QrCodeColorRepo.java
@@ -1,9 +1,17 @@
 package com.lagodich.textqrconvertor.repository;
 
 import com.lagodich.textqrconvertor.entity.QrCodeColor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface QrCodeColorRepo extends CrudRepository<QrCodeColor, Long> {
     QrCodeColor findByColor (String color);
     QrCodeColor findByBgcolor (String bgcolor);
+
+    @Query("SELECT qcc FROM QrCodeColor qcc WHERE qcc.qrCode.id = :userId")
+    List<QrCodeColor> findQrCodeColorById(@Param("userId") Long userId);
+
 }
